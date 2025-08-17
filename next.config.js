c556ae818d9f5d373ea889aed2b14f63828240a6
Florian Wahl <florian.wahl.31@gmail.com>
2025-08-17 15:43:28 -0400

    fix: set base path for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.BASE_PATH || '';
+
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
     images: {
         unoptimized: true
-    }
-}
+    },
+    basePath,
+    assetPrefix: basePath ? `${basePath}/` : undefined
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;

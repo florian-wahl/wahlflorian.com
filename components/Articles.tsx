c159e40157cab5e013bce8ba89f96f1f40d87859
Florian Wahl <florian.wahl.31@gmail.com>
2025-08-19 20:50:14 -0400

    Personalize section headings

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -31,7 +31,7 @@ const Articles: React.FC = () => {
         <section className="bg-white dark:bg-gray-800">
             <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
                 <h1 className="text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-                    Articles
+                    Articles by Florian Wahl
                 </h1>
             </div>
             {/* Grid starts here */}
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact: React.FC = () => {
         <section>
             <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800 antialiased">
                 <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-                    Contact
+                    Contact Florian Wahl
                 </h1>
             </div>
             <div className="rounded-md shadow-md bg-[#02044A] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">

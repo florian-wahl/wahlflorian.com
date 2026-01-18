7022caf25ee69b6e09f5a7bbbee71fd1b65a79a1
florian-wahl <florian.wahl.31@gmail.com>
2026-01-17 19:34:30 -0500

    "Expert Mode"

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero: React.FC = () => {
                     <div className="w-full md:w-1/2 text-center md:text-left">
                         <div className="mb-6">
                             <span className="inline-block px-4 py-2 bg-yellow-400 text-black font-bold font-mono text-sm pixel-border">
-                                LEVEL 1
+                                EXPERT MODE
                             </span>
                         </div>
                         <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold text-white mb-6 pixel-text">
